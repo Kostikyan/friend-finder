a6a845623367ba6b9ec6fa9d3dd2645612add226
User <artyom.styopayevich@gmail.com>
2023-06-07 00:40:06 +0400

    user-education

---
+++ src/main/java/com/friendfinder/repository/EducationRepository.java
@@ -0,0 +1,9 @@
+package com.friendfinder.repository;
+
+import com.friendfinder.entity.Education;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EducationRepository extends JpaRepository<Education, Integer> {
+
+
+}
