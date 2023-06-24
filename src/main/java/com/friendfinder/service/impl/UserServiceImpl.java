ba8e3b559c56967c67fa0fc4fa15464f32b6f4a1
David <davidkirakosyan504@gmail.com>
2023-06-24 17:22:32 +0400

    templates links

--- src/main/java/com/friendfinder/service/impl/UserServiceImpl.java
+++ src/main/java/com/friendfinder/service/impl/UserServiceImpl.java
@@ -4,11 +4,14 @@ import com.friendfinder.entity.Country;
 import com.friendfinder.entity.User;
 import com.friendfinder.repository.CountryRepository;
 import com.friendfinder.repository.UserRepository;
+import com.friendfinder.security.CurrentUser;
+import com.friendfinder.service.FriendRequestService;
 import com.friendfinder.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,12 +22,27 @@ public class UserServiceImpl implements UserService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final CountryRepository countryRepository;
+    private final FriendRequestService friendRequestService;
 
     @Override
     public List<User> userFindAll() {
         return userRepository.findAll();
     }
 
+
+    @Override
+    public List<User> userForAddFriend(CurrentUser currentUser) {
+        List<User> users = userFindAll();
+        List<User> userForAddFriend = new ArrayList<>();
+        for (User user : users) {
+            if (friendRequestService.findBySenderIdAndReceiverId(user.getId(), currentUser.getUser().getId()) == null &&
+                    user.getId() != currentUser.getUser().getId() && friendRequestService.findBySenderIdAndReceiverId(currentUser.getUser().getId(),
+                    user.getId()) == null) {
+                userForAddFriend.add(user);
+            }
+        }
+        return userForAddFriend;
+    }
     @Override
     public List<Country> findAllCountries() {
         return countryRepository.findAll();
